8752c9f3d9f3f4d2970c6cbee59e5589629357b7
Mike Rainey <mike.rainey@inria.fr>
2017-08-20 02:07:09 +0200

    working on language definition

--- redex/ippc.rkt
+++ redex/ippc.rkt
@@ -2,38 +2,55 @@
 (require redex)
 
 (define-language IPPC
-  (THD ::= (S CS PROG)) ; thread
-  (S ::= (stack F ...)) ; stack
-  (F ::= (frame FL T MV ...)) ; frame
-  (T ::= (LB LB)) ; trampoline (pred, succ)
-  (MV ::= number CSL) ; machine value
+  (THREAD ::= (STACK CHUNK-STORE PROGRAM)) ; thread
+  (STACK ::= (stack FRAME ...)) ; stack
+  (FRAME ::= (frame CFG-LABEL TRAMPOLINE (VAR MACHINE-VALUE) ...)) ; frame
+  (TRAMPOLINE ::= (BB-LABEL BB-LABEL)) ; trampoline: (pred, succ)
+  (MACHINE-VALUE ::= LITERAL CHUNK-STORE-LOCATION) ; machine value
 
-  (CS ::= ((CSL C) ...)) ; chunk store
-  (C ::= (chunk MV ...)) ; chunk
-  (CSL ::= number) ; chunk-store location
+  (CHUNK-STORE ::= ((CHUNK-STORE-LOCATION CHUNK) ...)) ; chunk store
+  (CHUNK ::=
+         (chunk-header CHUNK-STORE-LOCATION HASH ...)
+         (chunk MACHINE-VALUE ...))
+  (CHUNK-STORE-LOCATION ::= number) ; chunk-store location
+  (HASH ::= (hash number)) ; ipfs hash value
   
-  (PROG (CFG ...)) ; program
-  (CFG ::= (FL F (BB ...))) ; function (as CFG)
-  (BB ::= (LB IS)) ; basic block
-  (IS ::= (I ...))
-  (I ::= ; instruction
-     (let ((VAR (O V ...))))
-     (O V ...))
-  (O ::=
-     add1 sub1 + *
+  (PROGRAM ::= (program CFG ...)) ; whole program
+  (CFG ::= (cfg CFG-LABEL FRAME (BB ...))) ; function (as CFG)
+  (BB ::= (BB-LABEL INSTRS CONTROL-OP)) ; basic block
+  (CONTROL-OP ::=
+              (jump BB-LABEL)
+              (conditional-jump OPERAND BB-LABEL ...) ; (conditional-jump i lab ...)
+                                                      ; jump to ith basic-block label in lab ...
+              (call CFG-LABEL BB-LABEL OPERAND ...))  ; (call f lab args ...)
+                                                      ; call function f passing argument list
+                                                      ; args ..., and upon return jumping to
+                                                      ; basic block with label lab
+  (INSTRS ::= (INSTR ...))
+  (INSTR ::= ; instruction
+     (OP OPERAND ...))
+  (OP ::=
      chunk-alloc chunk-free
-     chunk-read chunk-write
-     frame-read frame-write)
-  (V ::=
-     VAR number)
-
-  (LB ::= variable-not-otherwise-mentioned) ; basic-block label
-  (FL ::= variable-not-otherwise-mentioned) ; function label
+     chunk-store frame-store)
+  (OPERAND ::=
+       MACHINE-VALUE
+       VAR ; lookup value in the frame
+       (chunk-load OPERAND OPERAND)) ; (chunk-read c i)
+                                     ; load value from position i in chunk c
+       
+  (LITERAL ::= number HASH)
+  (BB-LABEL ::= variable-not-otherwise-mentioned) ; basic-block label
+  (CFG-LABEL ::= variable-not-otherwise-mentioned) ; CFG/function label
   (VAR ::= variable-not-otherwise-mentioned)) ; variable
 
 (define-metafunction IPPC
-  lookup-basic-block : (BB ...) LB -> IS
-  [(lookup-basic-block ((LB_before IS_before) ... (LB_1 IS_1) (LB_after IS_after) ...) LB_1)
-   IS_1]
-  [(lookup-basic-block ((LB_before IS_before) ...) LB_1)
+  lookup-basic-block : (BB ...) BB-LABEL -> INSTRS
+  [(lookup-basic-block ((BB-LABEL_before INSTRS_before) ... (BB-LABEL_1 INSTRS_1) (BB-LABEL_after INSTRS_after) ...) BB-LABEL_1)
+   INSTRS_1]
+  [(lookup-basic-block ((BB-LABEL_before INSTRS_before) ...) BB-LABEL_1)
    ()])
+
+(define-metafunction IPPC
+  lookup-frame-variable : FRAME VAR -> MACHINE-VALUE
+  [(lookup-frame-variable (frame CFG-LABEL_1 TRAMPOLINE_1 (VAR_before MACHINE-VALUE_before) ... (VAR_1 MACHINE-VALUE_1) (VAR_after MACHINE-VALUE_after) ...) VAR_1)
+   MACHINE-VALUE_1])
